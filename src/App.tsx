7b494845eea8e426fcfd1bf862261ea2e91ae5e2
Guilherme <guiteixeira1926@gmail.com>
2024-07-02 13:41:26 -0400

    add project structure

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import FormAddInput from "./components/FormAddInput";
+import TaskList from "./components/TaskList";
+import Stats from "./components/Stats";
+
+export default function App() {
+  return (
+    <div>
+      <FormAddInput />
+      <TaskList />
+      <Stats />
+    </div>
+  );
+}
